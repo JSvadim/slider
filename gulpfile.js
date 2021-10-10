bcbf0c29298cc126085b9ed095655226bed1aa95
JSvadim <sadomazo323@gmail.ru>
2021-10-10 08:12:25 +0300

    files structure created

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+const gulp = require('gulp'),
+      { watch, series } = require('gulp'),
+      sass = require('gulp-sass')(require('sass'));
+
+function scss() {
+    return gulp.src('styles/**/style.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('app'));
+} 
+
+function watchFiles() {
+    watch('styles/**/*.scss', series(scss));
+}
+
+exports.build = series(scss);
+exports.watchFiles = series(watchFiles);
