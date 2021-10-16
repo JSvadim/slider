6adb087f2f54bd1b54022b58449eace2111ec652
JSvadim <sadomazo323@gmail.ru>
2021-10-16 22:57:53 +0300

    slides changing ability and restriction to overflow line node while dragging was added

--- app/index.js
+++ app/index.js
@@ -5,6 +5,7 @@ const slideClassName = 'gallery-slide';
 class Gallery {
     constructor(containerNode, options = {}) {
         this.containerNode = document.querySelector(containerNode);
+        this.currentSlide = 0;
         this.initialLineNodePosition = 0;
         this.handleHtml = this.handleHtml.bind(this);
         this.setSize = this.setSize.bind(this);
@@ -12,6 +13,7 @@ class Gallery {
         this.startDrag = this.startDrag.bind(this);
         this.dragging = this.dragging.bind(this);
         this.stopDrag = this.stopDrag.bind(this);
+        this.changeSlide = this.changeSlide.bind(this);
         this.handleHtml();
         this.setSize();
         this.setEvents();
@@ -32,11 +34,13 @@ class Gallery {
     }
 
     setSize() {
+        this.containerSizes = this.containerNode.getBoundingClientRect();
         this.size = this.slideNodes.length;
         this.slideNodes.forEach(slide => {
-            return slide.style.width = this.containerNode.getBoundingClientRect().width + 'px'
+            return slide.style.width = this.containerSizes.width + 'px'
         })
-        this.lineNode.style.width = this.containerNode.getBoundingClientRect().width * this.size + 'px'
+        this.lineNode.style.width = this.containerSizes.width * this.size + 'px'
+        this.maxDraggingValue = this.lineNode.getBoundingClientRect().width - this.containerSizes.width
     }
 
     setEvents() {
@@ -54,14 +58,73 @@ class Gallery {
 
     dragging() {
         this.shift = event.clientX - this.clickPosition;
-        this.lineNode.style.transform = `translateX(${this.shift + this.initialLineNodePosition}px)`;
+        this.currentLineNodePosition = this.shift + this.initialLineNodePosition;
+        if(this.currentLineNodePosition > 0 ||
+            this.currentLineNodePosition < -this.maxDraggingValue) {
+            return this.lineNode.style.transform = `translateX(${(this.shift / 5) + this.initialLineNodePosition}px)`;
+        }
+        this.lineNode.style.transform = `translateX(${this.currentLineNodePosition}px)`;
+        console.log(this.shift)
     }
 
     stopDrag() {
-        this.initialLineNodePosition += this.shift;
         window.removeEventListener('pointermove', this.dragging);
         window.removeEventListener('pointerup', this.stopDrag);
         document.body.style.removeProperty('cursor');
+        // if overflow left
+        if(this.currentLineNodePosition > 0) {
+            this.lineNode.style.transition = "all 0.5s";
+            this.lineNode.style.transform = `translateX(0px)`;
+            this.initialLineNodePosition = 0;
+            return setTimeout(() => {
+                this.lineNode.style.removeProperty('transition');
+            }, 500);
+        }
+        // if overflow right
+        if(this.currentLineNodePosition < -this.maxDraggingValue) {
+            this.lineNode.style.transition = "all 0.5s";
+            this.lineNode.style.transform = `translateX(${-this.maxDraggingValue}px)`;
+            this.initialLineNodePosition = -this.maxDraggingValue;
+            return setTimeout(() => {
+                this.lineNode.style.removeProperty('transition');
+            }, 500); 
+        }
+        // if swipe was not enough to change slide
+        if(this.shift < 50 && this.shift > -50) {
+            this.lineNode.style.transition = "all 0.5s";
+            this.lineNode.style.transform = `translateX(${this.initialLineNodePosition}px)`;
+            return setTimeout(() => {
+                this.lineNode.style.removeProperty('transition');
+            }, 500); 
+        }
+        // next slide
+        if(this.shift < 0) {
+            return this.changeSlide('next');
+        }
+        // previous slide
+        if(this.shift > 0) {
+            return this.changeSlide('previous')
+        }
+    }
+
+    changeSlide(slideToChangeWith) {
+        this.lineNode.style.transition = "all 0.5s";
+        if(slideToChangeWith === 'next') {
+            this.currentSlide += 1;
+            const newPos = -this.containerSizes.width * this.currentSlide;
+            this.initialLineNodePosition = newPos;
+            this.lineNode.style.transform = `translateX(${newPos}px)`;
+            return setTimeout(() => {
+                this.lineNode.style.removeProperty('transition');
+            }, 500); 
+        }
+        this.currentSlide -= 1;
+        const newPos = -this.containerSizes.width * this.currentSlide;
+        this.initialLineNodePosition = newPos;
+        this.lineNode.style.transform = `translateX(${newPos}px)`;
+        setTimeout(() => {
+            this.lineNode.style.removeProperty('transition');
+        }, 500); 
     }
 }
 
