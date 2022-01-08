32b9eac9accb21f72eccb284629f85d92458947e
JSvadim <sadomazo323@gmail.com>
2022-01-08 11:15:49 +0300

    Fake slider animations
    I added animation for fake slider on the first screen.
    Also hover effects for buttons in get-it section.

--- landing-page/index.js
+++ landing-page/index.js
@@ -1,14 +1,19 @@
 
+
+
+//SLIDERS INITIALIZING START
 const sliderSpaceBetween = new Gallery("#spaceBetween", {
     spaceBetweenSlides: 20,
     arrows: true
-})
+});
 const sliderVerticalAlign = new Gallery("#verticalAlign", {
     spaceBetweenSlides: 30,
     vertical: true,
     arrows: true
-})
+});
 const sliderDots = new Gallery("#dots", {
     spaceBetweenSlides: 30,
     dots: true
-})
+});
+//SLIDERS INITIALIZING END
+
