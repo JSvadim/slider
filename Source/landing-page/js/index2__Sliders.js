887bdb0a6c521ce7d6ab3da70d51858812ec4389
JSvadim <sadomazo323@gmail.com>
2022-01-09 13:45:45 +0300

    Arrows animation added.
    I added animation for all arrows on landing page.

--- Source/landing-page/js/index2__Sliders.js
+++ Source/landing-page/js/index2__Sliders.js
@@ -3,16 +3,18 @@
 //SLIDERS INITIALIZING START
 const sliderSpaceBetween = new Gallery("#spaceBetween", {
     spaceBetweenSlides: 20,
-    arrows: true
+    arrows: true,
+    timeBetweenSlides: 1000,
 });
 const sliderVerticalAlign = new Gallery("#verticalAlign", {
     spaceBetweenSlides: 30,
     vertical: true,
-    arrows: true
+    arrows: true,
+    timeBetweenSlides: 1000,
 });
 const sliderDots = new Gallery("#dots", {
     spaceBetweenSlides: 30,
-    dots: true
+    dots: true,
 });
 //SLIDERS INITIALIZING END
 
