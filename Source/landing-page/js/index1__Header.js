584adb7f814b5659c090576cf1aafecf1d46a50f
JSvadim <sadomazo323@gmail.com>
2022-01-15 10:02:23 +0300

    Header added.
    I added animated header with navigation for landing page.

--- Source/landing-page/js/index1__Header.js
+++ Source/landing-page/js/index1__Header.js
@@ -79,30 +79,3 @@ function animateMenuItems(startingTime, isAnimating = true ) {
         item.style.animationFillMode = "forwards";
     }
 }
-
-
-//SLIDERS INITIALIZING START
-const sliderSpaceBetween = new Gallery("#spaceBetween", {
-    spaceBetweenSlides: 20,
-    arrows: true,
-    timeBetweenSlides: 1000,
-});
-const screenReader = new Gallery("#screenReader", {
-    spaceBetweenSlides: 20,
-    arrows: true,
-    dots: true,
-    timeBetweenSlides: 1000,
-    initialSlide: 2,
-});
-const sliderVerticalAlign = new Gallery("#verticalAlign", {
-    spaceBetweenSlides: 30,
-    vertical: true,
-    arrows: true,
-    timeBetweenSlides: 1000,
-});
-const sliderDots = new Gallery("#dots", {
-    spaceBetweenSlides: 30,
-    dots: true,
-});
-//SLIDERS INITIALIZING END
-
